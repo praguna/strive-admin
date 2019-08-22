9ba3ed3241e40c7d67a118dd85ac073e16a80563
praguna <praguna18@hotmail.com>
2019-08-23 00:13:59 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProblemsComponent } from './problems/problems.component';
+import { ProblemDetailComponent } from './problem-detail/problem-detail.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'login' ,pathMatch:'full'},
+  {path:'login',component:LoginComponent},
+  {path:'problems',component:ProblemsComponent,canActivate : [AuthGuard]},
+  {path:'problems/:id',component:ProblemDetailComponent,canActivate : [AuthGuard]},
+  {path:'**',redirectTo:'/login',pathMatch:'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProblemsComponent } from './problems/problems.component';
+import {MatInputModule} from '@angular/material/input';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {MatIconModule} from '@angular/material/icon';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { ProblemDetailComponent } from './problem-detail/problem-detail.component';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import { HttpClientModule } from '@angular/common/http';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { LoginComponent } from './login/login.component';
+import { TopbarComponent } from './topbar/topbar.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProblemsComponent,
+    ProblemDetailComponent,
+    LoginComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatInputModule,
+    MatSortModule,
+    MatTableModule,
+    MatPaginatorModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    HttpClientModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
